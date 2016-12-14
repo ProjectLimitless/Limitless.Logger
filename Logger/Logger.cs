571b2a25ecdfe9de82ec4093ba5b291d44f2ae3e
Donovan Solms <donovan.solms@gmail.com>
2016-12-14 17:17:23 +0200

    Added project and AssemblyInfo

---
+++ Logger/Logger.cs
@@ -0,0 +1,76 @@
+﻿/** 
+* This file is part of Project Limitless.
+* Copyright © 2016 Donovan Solms.
+* Project Limitless
+* https://www.projectlimitless.io
+* 
+* Project Limitless is free software: you can redistribute it and/or modify
+* it under the terms of the Apache License Version 2.0.
+* 
+* You should have received a copy of the Apache License Version 2.0 with
+* Project Limitless. If not, see http://www.apache.org/licenses/LICENSE-2.0.
+*/
+
+using System;
+
+using Limitless.Runtime.Interfaces;
+
+namespace Limitless.Logger
+{
+    public class Logger : ILogger
+    {
+        /// <summary>
+        /// Implemented from interface 
+        /// <see cref="Limitless.Runtime.Interfaces.ILogger.Trace(string, object[])"/>
+        /// </summary>
+        public void Trace(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Implemented from interface 
+        /// <see cref="Limitless.Runtime.Interfaces.ILogger.Debug(string, object[])"/>
+        /// </summary>
+        public void Debug(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Implemented from interface 
+        /// <see cref="Limitless.Runtime.Interfaces.ILogger.Info(string, object[])"/>
+        /// </summary>
+        public void Info(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Implemented from interface 
+        /// <see cref="Limitless.Runtime.Interfaces.ILogger.Warning(string, object[])"/>
+        /// </summary>
+        public void Warning(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Implemented from interface 
+        /// <see cref="Limitless.Runtime.Interfaces.ILogger.Error(string, object[])"/>
+        /// </summary>
+        public void Error(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Implemented from interface 
+        /// <see cref="Limitless.Runtime.Interfaces.ILogger.Critical(string, object[])"/>
+        /// </summary>
+        public void Critical(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Logger/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Limitless.Logger")]
+[assembly: AssemblyDescription("An NLog-based default logger for Project Limitless")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Project Limitless")]
+[assembly: AssemblyProduct("Limitless.Runtime")]
+[assembly: AssemblyCopyright("Copyright © Donovan Solms 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2ae440b4-2d16-4941-8e8b-049ac897d986")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
